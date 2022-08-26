cac6fa700fba90adbb8f31dc834888c5c6f0a45d
GunHoi <lgh509@kyonggi.ac.kr>
2022-08-26 20:13:36 +0900

    약수의 개수와 덧셈

---
+++ 약수의 개수와 덧셈.js
@@ -0,0 +1,29 @@
+function solution(left, right) {
+    var answer = 0;
+    for(let i=left;i<=right;i++){
+        let count = 0;
+        for(let j=1;j<=i;j++){
+            if(i%j===0){ count+=1; }
+        }
+        if(count%2===0){ answer+=i; }
+        else { answer-=i; }
+    }
+    return answer;
+}
+
+/* 다른 사람 풀이 best
+
+Tip! "제곱근이 정수면 약수의 개수가 홀수다"
+
+function solution(left, right) {
+    var answer = 0;
+    for (let i = left; i <= right; i++) {
+        if (Number.isInteger(Math.sqrt(i))) {
+            answer -= i;
+        } else {
+            answer += i;
+        }
+    }
+    return answer;
+}
+*/
