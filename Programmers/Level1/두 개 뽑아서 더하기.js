fe59e0c9767f3eaf4e3ab88975769c04d7338098
GunHoi <lgh509@kyonggi.ac.kr>
2022-08-27 12:52:46 +0900

    두 개 뽑아서 더하기 안되는 이유 설명 추가

--- Programmers/Level1/두 개 뽑아서 더하기.js
+++ Programmers/Level1/두 개 뽑아서 더하기.js
@@ -12,6 +12,8 @@ function solution(numbers) {
 /*
 아래와 같은 코드로 했을 때는 실패하였는데 이유가 뭘까??
 
+-> 정렬을 먼저 하고 시작하더라도, 조합의 덧셈 합에 의해서 오름차순이 깨질 수 있다.
+
 function solution(numbers) {
     let answer = [];
     let temp = numbers.sort();
@@ -22,5 +24,23 @@ function solution(numbers) {
     }
     const set = new Set(answer);
     return [...set]
+}
+
+그래서 위의 코드를 아래 코드처럼 다시 정렬해준다면 통과이다.
+ => 결론 : 어차피 조합의 합을 구하는 경우에서 순서는 중요하지 않으니
+ 앞에서 정렬을 하지말고, 뒤에서 마지막에 한 번에 하는 것이 좋다.
+
+function solution(numbers) {
+    let answer = [];
+    let temp = numbers.sort();  //정렬
+    for(let i=0;i<temp.length-1;i++){
+        for(let j=i+1;j<temp.length;j++){
+            answer.push(temp[i]+temp[j]);   //조합합
+        }
+    }
+    const set = new Set(answer);    //중복제거
+    let a = [...set];
+    a = a.sort((a,b)=>a-b);     //또 다시 정렬
+    return a
 }
 */
