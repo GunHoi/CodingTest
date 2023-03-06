21016bf66ccd9312c1fd6c8bf3f6d45ce6e3b32d
GunHoi <lgh509@kyonggi.ac.kr>
2023-03-06 17:23:29 +0900

    create baekjoon_algorithm

---
+++ baekjoon_codingTest/src/baekjoon_codingTest/MMain.java
@@ -0,0 +1,10 @@
+package baekjoon_codingTest;
+
+public class MMain {
+	public static void main(String[] args) {
+		double range = Math.sqrt(8);
+		System.out.println(range);
+		double a = Math.floor(Math.sqrt(8));
+		System.out.println(a);
+	}
+}
